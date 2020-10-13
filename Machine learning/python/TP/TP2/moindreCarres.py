601415fa3805247a83de28426c16b6f179c9b1a9
SaPetit <48793387+SaPetit@users.noreply.github.com>
2020-10-13 18:08:57 +0200

    import

--- Machine learning/python/TP/TP2/moindreCarres.py
+++ Machine learning/python/TP/TP2/moindreCarres.py
@@ -2,5 +2,21 @@
 
 import numpy as np
 import pylab as pl
+import matplotlib.pyplot as mp
 
+## Inplementation de l'algoritme permettant de recuperer la régression à partir d'une liste de données (x_i,y_i)
+import numpy as np
 
+data = np.loadtxt('http://stephane.ayache.perso.luminy.univ-amu.fr/zoom/cours/Cours/AAuto/dataRegLin2D.txt')
+
+def regLin(x,y):
+    ones = np.ones(np.shape(x[0]),1)
+    xt = np.transpose(x)
+    xtx = np.dot(xt,x)
+    inv = np.linalg.inv(xtx)
+    xty = np.dot(xt,y)
+    return np.dot(inv,xty)
+
+test = regLin(data[:,[0,1]],data[:,-1])
+
+print(test)
