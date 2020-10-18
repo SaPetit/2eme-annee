4c7c3b5e2501e041a625cad97cd1b4480e7ff08d
Lohysor <sacha.a.petit@gmail.com>
2020-10-18 03:00:03 +0200

    dodo samedi soir

--- MachineLearning/python/TP/TP2/FonctionTP2.py
+++ MachineLearning/python/TP/TP2/FonctionTP2.py
@@ -1,6 +1,7 @@
 #Algorithme Régression linéaire par moindres carrés
 
 from matplotlib.pyplot import subplot
+from matplotlib import cm
 import numpy as np
 from numpy.core.multiarray import result_type
 import pylab as pl
@@ -9,24 +10,22 @@ import math
 
 ## Inplementation de l'algoritme permettant de recuperer la régression à partir d'une liste de données (x_i,y_i)
 
-data = np.loadtxt('http://stephane.ayache.perso.luminy.univ-amu.fr/zoom/cours/Cours/AAuto/dataRegLin2D.txt')
-
-def planEnFonctionVecteurPonderation(sub_plot,W):
+def planEnFonctionVecteurPonderation(sub_plot,W, zOrder = 0):
     """
     Desine le plan normal à W dans la sous figure sub_plot.
     """
     if W.shape[0] == 3:
         a,b,d = W[0], W[1], W[-1]
 
-        x_ = np.linspace(-1,1,10)
-        y_ = np.linspace(-1,1,10)
+        x_ = np.linspace(-2,2,10)
+        y_ = np.linspace(-2,2,10)
 
         X_,Y_ = np.meshgrid(x_,y_)
         Z_ = d + a*X_ + b*Y_
 
-        surf = sub_plot.plot_surface(X_, Y_, Z_)
+        sub_plot.plot_surface(X_, Y_, Z_, cmap=cm.plasma, zorder = zOrder)
         pass
-    if W.shape[0] == 2:
+    elif W.shape[0] == 2:
         a,b,d = W[0], W[-1]
 
         x_ = np.linspace(-1,1,10)
@@ -35,19 +34,16 @@ def planEnFonctionVecteurPonderation(sub_plot,W):
         X_,Y_ = np.meshgrid(x_,y_)
         Z_ =  a*X_ + b*Y_
 
-        surf = sub_plot.plot_surface(X_, Y_, Z_)
+        sub_plot.plot_surface(X_, Y_, Z_)
     else:
         print("Le vecteur de ponderation ne comporte pas le bon nombre de paramètrd")
     pass
 
-def nuageDePoints(parameter_list):
-    """
-    docstring
-    """
-    
-    pass
-
 def rss(vecteurPonderation, X, Y):
+    """
+    Calcul la somme des moindres carrée (Residual sum of squares).
+    ensemble de données X de valeurs Y
+    """
     nombreDeValeurs = X.shape[0]
     resultat = 0
     for indice in range(0,nombreDeValeurs):
@@ -59,6 +55,10 @@ def rss(vecteurPonderation, X, Y):
     return resultat
 
 def mse(vecteurPonderation, X, Y):
+    """
+    Calcul la moyenne des moindres carrée (mean-square error).
+    ensemble de données X de valeurs Y
+    """
     resultat = rss(vecteurPonderation, X, Y)/ X.shape[0]
     return resultat
 
@@ -67,6 +67,10 @@ def rmse(vecteurPonderation, X, Y):
     return resultat
 
 def regLin(x,y):
+    """
+    Regression Linéaire avec biais.
+    ensemble de données x de valeurs y
+    """
     x = np.c_[x,np.ones(x.shape[0])]
     xt = np.transpose(x)
     xtx = np.dot(xt,x)
@@ -74,7 +78,13 @@ def regLin(x,y):
     xty = np.dot(xt,y)
     return np.dot(inv,xty)
 
-def graphRegLin2d(x,y,a=-5,b=5, imprimer = True, afficherDroite = True, afficherNuageDePoints = True, afficherErreur = True):
+def graphRegLin2d(x,y,a=-5,b=5, imprimer = False, afficherDroite = False, afficherNuageDePoints = False, afficherErreur = False, Tout = False):
+    """
+    Representation graphique de la régression linéaire avec biais.
+    ensemble de données x de valeurs y
+    a et b : abscisses min et max du segment représentant l'approximation affine.
+    Les parametres booleens imprimer, afficherDroite, afficherErreur, afficherNuageDePoints permette de choisir ce que l'on affiche.
+    """
     reg = regLin(x,y)
 
     if afficherNuageDePoints: pl.scatter(x[:, 0], y)
@@ -82,7 +92,12 @@ def graphRegLin2d(x,y,a=-5,b=5, imprimer = True, afficherDroite = True, afficher
     if afficherErreur : print(mse(reg,x,y)) 
     if imprimer: pl.show()
 
-def graphRegLin3d(x,y,a=-5,b=5, imprimer = True, afficherDroite = True, afficherNuageDePoints = True, afficherErreur = True):
+def graphRegLin3d(x,y,a=-5,b=5, imprimer = False, afficherDroite = False, afficherNuageDePoints = False, afficherErreur = False):
+    """
+    Representation graphique de la régression linéaire 3d avec biais.
+    ensemble de données x de valeurs y
+    Les parametres booleens imprimer, afficherDroite, afficherErreur, afficherNuageDePoints permette de choisir ce que l'on affiche.
+    """
     fig = pl.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.set_xlabel('X Label')
@@ -90,14 +105,15 @@ def graphRegLin3d(x,y,a=-5,b=5, imprimer = True, afficherDroite = True, afficher
     ax.set_zlabel('Z Label')
 
     regressionLineaire = regLin(x,y)
-    if afficherDroite :planEnFonctionVecteurPonderation(ax, regressionLineaire)
-    if afficherNuageDePoints: ax.scatter(x[:,0], x[:,1], y)
+    if afficherNuageDePoints: ax.scatter(x[:,0], x[:,1], y, s=50 )
+    if afficherDroite :planEnFonctionVecteurPonderation(ax, regressionLineaire, zOrder = 2)
     if afficherErreur : print(mse(regressionLineaire,x,y)) 
     if imprimer: pl.show()
 
-def graphRegLin(x,y, print = True, erreur = True, nuageDePoints = True, droite = True):
-    if x.shape[1] == 1:graphRegLin2d(x,y, imprimer = True, afficherDroite = True, afficherNuageDePoints = True, afficherErreur = True)
-    if x.shape[1] == 2:graphRegLin3d(x,y, imprimer = True, afficherDroite = True, afficherNuageDePoints = True, afficherErreur = True)
+def graphRegLin(x,y, a=-5,b=-5,imprimer = False, afficherDroite = False, afficherNuageDePoints = False, afficherErreur = False, Tout = False):
+    if Tout: imprimer, afficherDroite, afficherErreur, afficherNuageDePoints = True, True, True, True
+    if x.shape[1] == 1:graphRegLin2d(x,y,a=a,b=b, imprimer = imprimer, afficherDroite = afficherDroite, afficherNuageDePoints = afficherNuageDePoints, afficherErreur = afficherErreur)
+    if x.shape[1] == 2:graphRegLin3d(x,y, imprimer = imprimer, afficherDroite = afficherDroite, afficherNuageDePoints = afficherNuageDePoints, afficherErreur = afficherNuageDePoints)
 
 
 
---
+++ MachineLearning/python/TP/TP2/partie1.py
@@ -0,0 +1,20 @@
+from matplotlib.pyplot import plot
+import numpy as np
+import pylab as pl
+import matplotlib.pyplot as mp
+from FonctionTP2 import *
+
+data = np.loadtxt('http://stephane.ayache.perso.luminy.univ-amu.fr/zoom/cours/Cours/AAuto/dataRegLin2D.txt')
+
+X = data[:,0:2]
+X_0 = data[:,0:1]
+X_1 = data[:,1:2]
+Y = data[:,-1]
+
+# print("\nVecteur de ponderation :\n", regLin(X,Y))
+# print("\nErreur de regression lineaire selon la premiere coordonnée :")
+# graphRegLin(X_0,Y, a=-3,b=3, Tout = True)
+# print("\nErreur de regression lineaire selon la seconde coordonnée :")
+# graphRegLin(X_1,Y, a=-3,b=3, Tout = True)
+print("\nErreur de regression lineaire selon les 2 coordonnées :")
+graphRegLin(X,Y, Tout = True)
